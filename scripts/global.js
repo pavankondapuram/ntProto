b5301feae2c4868ceca1814504bac18ef68264bf
Pavan Kondapuram <pavan@graphicana.co.in>
2012-08-22 23:01:47 +0100

    dojo initiated

--- scripts/global.js
+++ scripts/global.js
@@ -1,3 +1,18 @@
 dojo.ready(function(){
-	
+
+	require(["scripts/dojo/dojo/NodeList-manipulate"]);
+
+	var selectedCity = '';
+
+
+	array.forEach(
+		var selectedCity = dojo.query('.citySelection a').innerHTML();
+		dojo.query('.citySelection a').attr('href',selectedCity);
+	);
+
+	dojo.query('.citySelection').style('display','block');
+
+	dojo.query('.citySelection a').onclick(function(){
+		dojo.query('.citySelection').style('display','none');
+	})
 })
