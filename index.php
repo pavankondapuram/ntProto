94a6d4df9d4c13c37749ba4e181c08cb7d25c3f5
arjun <arjun@thetechaxis.com>
2012-09-15 13:54:18 +0530

    new index.php added

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+	<link rel="stylesheet/less" type="text/css" href="styles/styles.less">
+</head>
+<body>
+  <div id="wrapper">
+    <header>
+      <div id="logo">LOGO</div>
+      <div class="headerRight">
+        <div class="tour">
+          <div class="title">New to Namma Tumakuru?</div>
+          <div class="MbuttonGreen">Take a quick tour</div>
+          <div class="MbuttonGreen">Guest Login</div>
+        </div>
+        <div id="registeredUser">
+          <div class="title">Registered user?</div>
+          <div class="MbuttonGreen">Login</div>
+        </div>
+        <div id="profile">
+          <div class="name">First LastName</div>
+          <div class="image"></div>
+          <div class="logGroup">
+            <div class="MbuttonRed">Logout</div>
+            <div class="settings">Settings</div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <div id="container">
+      <div class="citySelection">
+        <div class="title">Select your city</div>
+        <ul>
+          <a class="city">Bengaluru</a>
+          <a class="city">Tumakuru</a>
+          <a class="city">Mysuru</a>
+          <a class="city">Belagavi</a>
+          <a class="city">Hubbala</a>
+          <a class="city">Shimoga</a>
+          <a class="city">Mangaluru</a>
+          <a class="city">Davanagere</a>
+          <a class="city">Dharwad</a>
+        </ul>
+      </div>
+      <nav>
+        <ul>
+          <li id="business">Business</li>
+          <li id="tourism">Tourism</li>
+          <li id="offers">Offers</li>
+          <li id="services">Services</li>
+        </ul>
+      </nav>
+      <div id="search">
+        <div class="title">Search</div>
+        <input type="">
+        <button class="submit">Search</button>
+      </div>
+    </div>
+    <footer>
+      <ul id="social">
+        <li class="facebook">Facebook</li>
+        <li class="googlePlus">Google plus</li>
+        <li class="twitter">Twitter</li>
+        <li class="rss">RSS</li>
+      </ul>
+      <ul class="menu">
+        <li class="">About XSP</li>
+        <li class="">About Namma Tumakuru</li>
+        <li class="">Team</li>
+        <li class="">Jobs</li>
+        <li class="">Contact us</li>
+      </ul>
+    </footer>
+  </div>
+	<script src="scripts/dojo1.8.0.min.js"></script>
+  <script src="scripts/less1.3.0.min.js" type="text/javascript"></script>
+  <script src="scripts/global.js"></script>  
+</body>
+</html>
